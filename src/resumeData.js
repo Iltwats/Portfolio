4a375cafab51b87390df227626682951cd03f29a
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2020-06-20 23:40:10 +0530

    added source

---
+++ src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+  "imagebaseurl": "https://media-exp1.licdn.com/dms/image/C5603AQGbMvNAYhCzUw/profile-displayphoto-shrink_400_400/0?e=1597881600&v=beta&t=UF9Lq4eON6kIG2GjcTEyKGcpc4CdmvyegqP-ij9brb8",
+  "name": "Atul Sharma",
+  "role": "Software Developer",
+  "linkedinId": "https://www.linkedin.com/1220atul",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [{
+      "name": "Linkedin",
+      "url": "https://www.linkedin.com/1220atul",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "Github",
+      "url": "https://github.com/Iltwats",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "Twitter",
+      "url":"https://twitter.com/Iltwats_Atul",
+      "className":"fa fa-twitter"
+    },
+    {
+      "name":"Instagram",
+      "url":"https://instagram.com/atulsharma_01",
+      "className": "fa fa-instagram"
+    }
+
+  ],
+  "aboutme": "Passionate problem solver pursuing a bachelor's degree focused in Computer Software Engineering from JECRC University. \n\nI love Solving community problems around myself with code and help my peers learn about new tech advancement as a responsible Microsoft Student Partner with a motto to code, collaborate and share.\n\nA creative team leader with interpersonal skills, a problem-solving attitude and a love for Basketball and Music..",
+  "address": "India",
+  "website": "https://www.techfact.codes",
+  "education": [{
+    "UniversityName": "JECRC University",
+    "specialization": "Bachelor`s Of Technology",
+    "MonthOfPassing": "Jun",
+    "YearOfPassing": "2023"
+  }],
+  "work": [{
+    "CompanyName": "Microsoft",
+    "specialization": "Student Partner",
+    "MonthOfLeaving": "Jan",
+    "YearOfLeaving": "2020",
+
+  }],
+  "skillsDescription": "Your skills here",
+  "skills": [{
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "C++"
+    }
+  ],
+  "portfolio": [{
+      "name": "Pokedox",
+      "description": "Checkout the universe Pokemons in the Pokedox.",
+      "imgurl": "https://he-s3.s3.amazonaws.com/media/uploads/0f9e056.jpg"
+    },
+    {
+      "name": "Credit Card Validator",
+      "description": "Check if your credit card is valid or not.",
+      "imgurl": "https://windows-cdn.softpedia.com/screenshots/Shaikh-Credit-Card-Validator_1.png"
+    },
+    {
+      "name": "NumberGuesser-Game",
+      "description": "A number guessing game between computer and user.",
+      "imgurl": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQe4QWw7iwzYRNamZWF72BvVuxwTvQ3P5GTmxYuUxJzOloBt62W&usqp=CAU"
+    },
+    {
+      "name": "Google Scrapper",
+      "description": "Google Scraper & Mass Exploiter",
+      "imgurl": "https://techcrunch.com/wp-content/uploads/2015/10/screen-shot-2015-10-08-at-4-20-16-pm.png?w=730&crop=1"
+    }
+  ],
+}
+
+export default resumeData
